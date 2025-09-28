3f6710de090cddd8ee4f300997305674ed66c83d
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-28 14:27:31 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,21 +3,6 @@ const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
-  "./grandeursCosmiques.js",
-  "./modulesCapteurs.js",
-  "./rituels.js",
-  "./data/config.json",
-  "./data/journal.json",
-  "./assets/soleil.svg",
-  "./assets/lune.svg",
-  "./assets/halo_souffle.svg",
-  "./assets/halo_clarte.svg",
-  "./assets/halo_rituel.svg",
-  "./assets/souffle.mp3",
-  "./assets/clarte.mp3",
-  "./assets/rituel.mp3",
-  "./index.html",
-  "./style.css",
   "./js/grandeursCosmiques.js",
   "./js/modulesCapteurs.js",
   "./js/rituels.js",
@@ -32,17 +17,12 @@ const ASSETS = [
   "./assets/souffle.mp3",
   "./assets/clarte.mp3",
   "./assets/rituel.mp3"
-  
 ];
 
-// Installation
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// Activation
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -51,9 +31,6 @@ self.addEventListener("activate", event => {
   );
 });
 
-// Fetch
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
