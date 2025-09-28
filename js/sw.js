328c224e4045c2f85aba86c5a4ff416ce28959bb
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-28 13:44:13 +0000

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "cockpit-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./grandeursCosmiques.js",
+  "./modulesCapteurs.js",
+  "./rituels.js",
+  "./data/config.json",
+  "./data/journal.json",
+  "./assets/soleil.svg",
+  "./assets/lune.svg",
+  "./assets/halo_souffle.svg",
+  "./assets/halo_clarte.svg",
+  "./assets/halo_rituel.svg",
+  "./assets/souffle.mp3",
+  "./assets/clarte.mp3",
+  "./assets/rituel.mp3"
+];
+
+// Installation
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activation
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
